3cd07e326b31725021d5b3f00b0344d563b3fbdc
Naveedahmedtech <technaveedahmed@gmail.com>
2023-05-16 13:07:48 +0500

    testing

--- src/pages/home/trending/TrendingCarousel.jsx
+++ src/pages/home/trending/TrendingCarousel.jsx
@@ -7,7 +7,6 @@ import { useSelector } from "react-redux";
 import { movieState } from "../../../features/movieSlice";
 const responsive = {
   superLargeDesktop: {
-    // the naming can be any, depends on you.
     breakpoint: { max: 4000, min: 3000 },
     items: 5,
   },
